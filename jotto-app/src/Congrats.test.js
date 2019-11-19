570389c7dcb82c62ddfd381c7e927614924c0b89
debbieellis7 <debbieellis7@gmail.com>
2019-11-19 18:26:25 +0800

    Congrats Tests and the .text() method

--- jotto-app/src/Congrats.test.js
+++ jotto-app/src/Congrats.test.js
@@ -17,13 +17,22 @@ const setup = (props={}) => {
 }
 
 test('renders without error', () => {
+  const wrapper = setup()
+  const component = findByTestAttr(wrapper, 'component-congrats')
 
+  expect(component.length).toBe(1)
 })
 
 test('renders no text when `success` prop is false', () => {
+  const wrapper = setup({ success: false })
+  const component = findByTestAttr(wrapper, 'component-congrats')
 
+  expect(component.text()).toBe('')
 })
 
 test('renders non-empty congrats message when `success` prop is true', () => {
+  const wrapper = setup({ success: true })
+  const message = findByTestAttr(wrapper, 'congrats-message')
 
+  expect(message.text().length).not.toBe(0)
 })
--- jotto-app/test/testUtils.js
+++ jotto-app/test/testUtils.js
@@ -5,5 +5,5 @@
  * @returns {ShallowWrapper}
  */
 export const findByTestAttr = (wrapper, val) => {
-  return wrapper.find(`[data-test=${val}]`)
+  return wrapper.find(`[data-test="${val}"]`)
 }
