ab7db1beb4f9dcef38a01916f4b25970f8110165
debbieellis7 <debbieellis7@gmail.com>
2019-11-19 08:31:41 +0800

    Set up Congrats Component

---
+++ jotto-app/src/Congrats.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+/**
+ * Functional react component for congratulatory message.
+ * @function
+ * @returns {JSX.Element} = Rendered component (or null if `success` prop is false)
+ */
+export default () => {
+  return (
+    <div></div>
+  )
+}
