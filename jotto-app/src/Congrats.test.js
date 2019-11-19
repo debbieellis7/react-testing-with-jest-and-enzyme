0133d14d7e22a518acb37068e46e85c24b17cdaf
debbieellis7 <debbieellis7@gmail.com>
2019-11-19 16:34:04 +0800

    Congrats Component Test Setup

--- jotto-app/src/Congrats.test.js
+++ jotto-app/src/Congrats.test.js
@@ -1,6 +1,29 @@
 import React from 'react'
 import Enzyme, { shallow } from 'enzyme'
 import EnzymeAdapter from 'enzyme-adapter-react-16'
+import { findByTestAttr } from '../test/testUtils'
 import Congrats from './Congrats'
 
 Enzyme.configure({ adapter: new EnzymeAdapter() })
+
+/**
+ * Factory function to create a ShallowWrapper for the Congrats component.
+ * @function setup
+ * @param {object} props - Component props specific to this setup
+ * @returns {ShallowWrapper}
+ */
+const setup = (props={}) => {
+  return shallow(<Congrats {...props} />)
+}
+
+test('renders without error', () => {
+
+})
+
+test('renders no text when `success` prop is false', () => {
+
+})
+
+test('renders non-empty congrats message when `success` prop is true', () => {
+
+})
