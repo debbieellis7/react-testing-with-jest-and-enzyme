6a6913dae7170ef1b86edcded287ab8217ffc01c
debbieellis7 <debbieellis7@gmail.com>
2019-11-21 07:00:54 +0800

    Add `secretWord` Action Creator, Reducer and Tests

--- jotto-app/src/actions/index.js
+++ jotto-app/src/actions/index.js
@@ -31,6 +31,12 @@ export const guessWord = (guessedWord) => {
 
 export const getSecretWord = () => {
   return (dispatch) => {
-    axios.get('http://localhost:3030')
+    return axios.get('http://localhost:3030')
+      .then(response => {
+        dispatch({
+          type: actionTypes.SET_SECRET_WORD,
+          payload: response.data
+        })
+      })
   }
 }
