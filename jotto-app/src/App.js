4085c23500e4562b7c5739a610749b780a04b453
debbieellis7 <debbieellis7@gmail.com>
2019-11-20 10:20:11 +0800

    Manual Acceptance Testing

--- jotto-app/src/App.js
+++ jotto-app/src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import GuessedWords from './GuessedWords'
+import Congrats from './Congrats'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <h1>Jotto</h1>
+        <Congrats success={true} />
+        <GuessedWords guessedWords={[
+          { guessedWord:'train', letterMatchCount: 3 }
+        ]}/>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
