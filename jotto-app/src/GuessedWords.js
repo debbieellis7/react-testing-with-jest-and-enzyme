ece9a237f4b8916ce4873d7b1062d5981fa928e3
debbieellis7 <debbieellis7@gmail.com>
2019-11-20 08:31:12 +0800

    Set up GuessedWords Component and Tests

---
+++ jotto-app/src/GuessedWords.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const GuessedWords = (props) => {
+  return (
+    <div />
+  )
+}
+
+GuessedWords.propTypes = {
+
+}
+
+export default GuessedWords
