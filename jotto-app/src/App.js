0412ab3edaf95e33b701474e6faabeffd5b47748
debbieellis7 <debbieellis7@gmail.com>
2019-11-20 10:26:32 +0800

    Bootstrap Styling

--- jotto-app/src/App.js
+++ jotto-app/src/App.js
@@ -6,7 +6,7 @@ import Congrats from './Congrats'
 class App extends Component {
   render() {
     return (
-      <div className='App'>
+      <div className='container'>
         <h1>Jotto</h1>
         <Congrats success={true} />
         <GuessedWords guessedWords={[
