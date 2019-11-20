51be7c13f89af93544d0c39a11a2b9a2d4576cac
debbieellis7 <debbieellis7@gmail.com>
2019-11-20 13:27:17 +0800

    Input Component Test Organization

---
+++ jotto-app/src/Input.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+class Input extends Component {
+  render() {
+    return <div />
+  }
+}
+
+const mapStateToProps = state => {
+  return {}
+}
+
+export default connect(mapStateToProps)(Input)
---
+++ jotto-app/src/Input.test.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import { findByTestAttr } from '../test/testUtils'
+import Input from './Input'
+
+describe('render', () => {
+  describe('word has not been guessed', () => {
+    test('renders component without error', () => {
+
+    })
+
+    test('renders input box', () => {
+
+    })
+
+    test('renders submit button', () => {
+
+    })
+ })
+
+  describe('word has been guessed', () => {
+    test('renders component without error', () => {
+
+    })
+
+    test('does not render input box', () => {
+
+    })
+
+    test('does not render submit button', () => {
+
+    })
+  })
+})
+
+describe('update state', () => {
+
+})
