f149f5d607b8f13b0fbc05b29fb9ec8b0befc2ad
debbieellis7 <debbieellis7@gmail.com>
2019-11-20 19:34:22 +0800

    Input Tests and Code for success=false

--- jotto-app/src/Input.js
+++ jotto-app/src/Input.js
@@ -3,12 +3,33 @@ import { connect } from 'react-redux'
 
 class Input extends Component {
   render() {
-    return <div />
+    const contents = this.props.success ? null : (
+      <form className='form-inline'>
+        <input
+          data-test='input-box'
+          className='mb-2 mx-sm-3'
+          type='text'
+          placeholder='enter guess'
+        />
+        <button
+          data-test='submit-button'
+          className='btn btn-primary mb-2'
+          type='submit'
+        >
+          Submit
+        </button>
+      </form>
+    )
+    return (
+      <div data-test='component-input'>
+        {contents}
+      </div>
+    )
   }
 }
 
-const mapStateToProps = state => {
-  return {}
+const mapStateToProps = ({ success }) => {
+  return { success }
 }
 
 export default connect(mapStateToProps)(Input)
