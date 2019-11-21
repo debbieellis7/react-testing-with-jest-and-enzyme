f5ec71aab3ee96629c128a5a8a9c549bb5e91406
debbieellis7 <debbieellis7@gmail.com>
2019-11-21 13:55:23 +0800

    Setting up Jotto for Hooks and Context

---
+++ jotto-app-redux/src/helpers/index.test.js
@@ -0,0 +1,23 @@
+import { getLetterMatchCount } from './index'
+
+describe('getLetterMatchCount', () => {
+  const secretWord = 'party'
+
+  test('returns correct count when there are no matching letters', () => {
+    const letterMatchCount = getLetterMatchCount('bones', secretWord)
+
+    expect(letterMatchCount).toBe(0)
+  })
+
+  test('returns the correct count where there are 3 matching letters', () => {
+    const letterMatchCount = getLetterMatchCount('train', secretWord)
+
+    expect(letterMatchCount).toBe(3)
+  })
+
+  test('returns correct count when there are duplicate letters in the guess', () => {
+    const letterMatchCount = getLetterMatchCount('parka', secretWord)
+
+    expect(letterMatchCount).toBe(3)
+  })
+})
