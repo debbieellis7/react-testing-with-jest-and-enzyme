d5400e64f237776855360081656d4ac8722f6a6f
debbieellis7 <debbieellis7@gmail.com>
2019-11-21 14:07:58 +0800

    Basic App Component

--- jotto-app-hooks-and-context/src/App.test.js
+++ jotto-app-hooks-and-context/src/App.test.js
@@ -1,9 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import { shallow } from 'enzyme'
+import { findByTestAttr } from '../test/testUtils'
+import App from './App'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+/**
+ * Setup function for app component.
+ * @returns {ShallowWrapper}
+ */
+const setup = () => {
+  return shallow(<App />)
+}
+
+test('App renders without error', () => {
+  const wrapper = setup()
+  const component = findByTestAttr(wrapper, 'component-app')
+
+  expect(component.length).toBe(1)
+})
