01440db196f2e4bdb16303a69b25a3e471f1738c
debbieellis7 <debbieellis7@gmail.com>
2019-11-22 08:37:06 +0800

    Prepare Input for Writing setState Tests

--- jotto-app-hooks-and-context/src/Input.js
+++ jotto-app-hooks-and-context/src/Input.js
@@ -2,8 +2,24 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 function Input({ secretWord  }) {
+  const [ currentGuess, setCurrentGuess ] = React.useState('')
+
   return (
     <div data-test='component-input'>
+      <form data-test='component-input'>
+        <input
+          data-test='input-box'
+          className='mb-2 mx-sm-3'
+          type='text'
+          placeholder='enter guess'
+        />
+        <button
+          data-test='submit-button'
+          className='btn btn-primary mb-2'
+         >
+          Submit
+        </button>
+      </form>
     </div>
   )
 }
