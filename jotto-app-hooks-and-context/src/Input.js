b75937ad3274842d035e6365eb325413462b55ef
debbieellis7 <debbieellis7@gmail.com>
2019-11-22 08:59:19 +0800

    State-Controlled Field

--- jotto-app-hooks-and-context/src/Input.js
+++ jotto-app-hooks-and-context/src/Input.js
@@ -6,16 +6,19 @@ function Input({ secretWord  }) {
 
   return (
     <div data-test='component-input'>
-      <form data-test='component-input'>
+      <form className='form-inline'>
         <input
           data-test='input-box'
           className='mb-2 mx-sm-3'
           type='text'
           placeholder='enter guess'
+          value={currentGuess}
+          onChange={e => setCurrentGuess(e.target.value)}
         />
         <button
           data-test='submit-button'
           className='btn btn-primary mb-2'
+          onClick={e => e.preventDefault()}
          >
           Submit
         </button>
--- jotto-app-hooks-and-context/src/Input.test.js
+++ jotto-app-hooks-and-context/src/Input.test.js
@@ -21,3 +21,19 @@ test('Input renders without error', () => {
 test('does not throw warning with expected props', () => {
   checkProps(Input, { secretWord: 'party' })
 })
+
+describe('state controlled input field', () => {
+
+  test('state updates with value of input box upon change', () => {
+    const mockSetCurrentGuess = jest.fn()
+    React.useState = jest.fn(() => ['', mockSetCurrentGuess])
+
+    const wrapper = setup()
+    const inputBox = findByTestAttr(wrapper, 'input-box')
+
+    const mockEvent = { target: { value: 'train' } }
+    inputBox.simulate('change', mockEvent)
+
+    expect(mockSetCurrentGuess).toHaveBeenCalledWith('train')
+  })
+})
