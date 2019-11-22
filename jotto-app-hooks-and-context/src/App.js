ddbae8637d4802a672042e2249c984ee2126704c
debbieellis7 <debbieellis7@gmail.com>
2019-11-22 10:43:40 +0800

    Code useEffect and useReducer to Pass Tests

--- jotto-app-hooks-and-context/src/App.js
+++ jotto-app-hooks-and-context/src/App.js
@@ -1,7 +1,41 @@
 import React from 'react'
 import './App.css'
+import hookActions from './actions/hookActions'
+
+/**
+ * Reducer to update state, called automatically by dispatch
+ * @param state {object} - existing state
+ * @param action {object} - contains 'type' and 'payload' properties for the state update
+ *      for example: { type: 'setSecretWord', payload: 'party' }
+ * @return {object} - new state
+ */
+function reducer(state, action) {
+  switch(action.type) {
+    case 'setSecretWord':
+      return {
+        ...state,
+        secretWord: action.payload
+      }
+    default:
+      throw new Error(`Invalid action type: ${action.type}`)
+  }
+}
 
 function App() {
+  const [state, dispatch] = React.useReducer(
+    reducer,
+    { secretWord: null }
+  )
+
+  const setSecretWord = secretWord => dispatch({
+    type: 'setSecretWord',
+    payload: secretWord
+  })
+
+  React.useEffect(
+    () => { hookActions.getSecretWord(setSecretWord) }, []
+  )
+
   return (
     <div data-test='component-app'>
     </div>
