648f26e05d1d21a7afe68265781375581899846d
debbieellis7 <debbieellis7@gmail.com>
2019-11-22 15:11:39 +0800

    LanguagePicker Component: Planning and Setup

---
+++ jotto-app-hooks-and-context/src/LanguagePicker.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function LanguagePicker({ setLanguage }) {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default LanguagePicker
---
+++ jotto-app-hooks-and-context/src/LanguagePicker.test.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import { findByTestAttr, checkProps } from '../test/testUtils'
+
+test('renders without error', () => {
+
+})
+
+test('does not throw warning with expected props', () => {
+
+})
+
+test('renders non-zero language icons', () => {
+
+})
+
+test('calls setLanguage prop upon click', () => {
+
+})
