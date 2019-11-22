afc6a90347903d1d58aabded1a0e2597d88f2eb4
debbieellis7 <debbieellis7@gmail.com>
2019-11-22 11:09:24 +0800

    secretWord Prop Warning for Input

--- jotto-app-hooks-and-context/src/App.test.js
+++ jotto-app-hooks-and-context/src/App.test.js
@@ -34,4 +34,15 @@ describe('getSecretWord calls', () => {
     // check to see if secret word was updated
     expect(mockGetSecretWord).toHaveBeenCalled()
   })
+
+  test('secretWord does not update on App update', () => {
+    const wrapper = setup()
+    mockGetSecretWord.mockClear()
+
+    // wrapper.update() doesn't trigger update
+    // (issue forked from https://github.com/airbnb/enzyme/issues/2091)
+    wrapper.setProps()
+
+    expect(mockGetSecretWord).not.toHaveBeenCalled()
+  })
 })
