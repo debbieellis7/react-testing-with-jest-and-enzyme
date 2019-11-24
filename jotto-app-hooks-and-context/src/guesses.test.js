e3edf96e4ef11f916aca61599370cf9be9f8ff06
debbieellis7 <debbieellis7@gmail.com>
2019-11-24 10:35:26 +0800

    Set Up Test for Input Setting GuessedWords Context

--- jotto-app-hooks-and-context/src/guesses.test.js
+++ jotto-app-hooks-and-context/src/guesses.test.js
@@ -1,18 +1,30 @@
 import React from 'react'
 import { mount } from 'enzyme'
 import { findByTestAttr } from '../test/testUtils'
+import guessedWordsContext from './contexts/guessedWordsContext'
 import successContext from './contexts/successContext'
 import Input from './Input'
+import GuessedWords from './GuessedWords'
 
-function setup(secretWord = 'party') {
+function setup(guessedWords = [], secretWord = 'party') {
   const wrapper = mount(
-    <successContext.SuccessProvider>
-      <Input secretWord={secretWord} />
-    </successContext.SuccessProvider>
+    <guessedWordsContext.GuessedWordsProvider>
+      <successContext.SuccessProvider>
+        <Input secretWord={secretWord} />
+        <GuessedWords />
+      </successContext.SuccessProvider>
+    </guessedWordsContext.GuessedWordsProvider>
   )
 
   const inputBox = findByTestAttr(wrapper, 'input-box')
   const submitButton = findByTestAttr(wrapper, 'submit-button')
+
+  // prepopulate guessedWords context by simulating word guess
+  guessedWords.map(word => {
+    const mockEvent = { target: { value: word } }
+    inputBox.simulate('change', mockEvent)
+    submitButton.simulate('click')
+  })
 
   return [wrapper, inputBox, submitButton]
 }
